5d69a588b430f6080b2938e3c02a9e956bdbdc7d
Cristian Luan Vieira <cristian.luanvieira@gmail.com>
2024-04-09 09:29:39 -0300

    first commit project

---
+++ src/main/java/com/cristian/project/api/ApiCovid19Application.java
@@ -0,0 +1,13 @@
+package com.cristian.project.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCovid19Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristian/project/api/ApiCovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.project.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCovid19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
